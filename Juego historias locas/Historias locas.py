1bc845f41af35facd70c882d926c7203cec41778
Aarón Ulloa <simiuc00@gmail.com>
2022-09-15 16:08:28 -0500

    mi primer juego en python

---
+++ Juego historias locas/Historias locas.py
@@ -0,0 +1,14 @@
+organización = "freecodecamp"
+
+#print("Aprende a programar con " + organización)
+#print("Aprende a programar con {}".format(organización))
+#print(f"Aprende a programar con {organización}") 
+
+adj = input("Adjetivo: ")
+verbo1 = input ("Verbo: ")
+verbo2 = input ("Verbo: ")
+sustantivo_plural = input ("Sustantivo(plural): ")
+
+madlib = f" ¡programar es tan {adj}! siempre me emociona, porque me encanta {verbo1} problemas. ¡Aprende a {verbo2} con freecodecamp y alcanza tus {sustantivo_plural}!"
+
+print(madlib)
